fea62cabaf4b7197336f17bb9a846aa70cc0b029
GeorgiyBelskiy <117345035+GeorgiyBelskiy@users.noreply.github.com>
2023-01-30 11:18:09 +0300

    task 3

---
+++ Task-3.py
@@ -0,0 +1,16 @@
+
+n = int(input())
+
+for i in range(n):
+    print(' ' * (n - i), end='')
+    print(' '.join(map(str, str(11 ** i))))
+
+
+
+# x = []
+# x = input("Введите, что необходимо посчитать:")
+# print((x))
+# if "*" in x:
+#     print(f"{x[0] * x[2]}")
+# else:
+#     print("NO")
